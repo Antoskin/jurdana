182c7ee7753ba2c37dba71b20341e6dfd70fedab
Antoskin <spinne92e@gmail.com>
2018-06-01 18:19:35 +0300

    v2

--- script/index.js
+++ script/index.js
@@ -1,3 +1,27 @@
-$('document').ready( () => {
-    console.log(`!`)
+import $ from 'jquery'
+import { openLeftSlide, openRightSlide, closeLeftSlide, closeRightSlide } from './slides/animation'
+
+
+$('document').ready( function() {
+    $(`.btn-left`).click( () => openLeftSlide() ) //open left
+    $(`.l-s-close`).click( () =>  closeLeftSlide() ) //close
+    $(`.btn-right`).click( () => {
+        openRightSlide()
+        $(`.send-btn-lef`).css({'opacity':0})
+    } )
+    $(`.r-s-close`).click( () =>  {
+        closeRightSlide()
+        setTimeout( () => {
+            $(`.send-btn-lef`).css({'opacity':1})
+        }, 666 )
+    } ) //close
+
+    $(`.send-btn-lef`).click( function() {
+        $(this).css({'opacity':'0'})
+
+        setTimeout( () => {
+            openRightSlide()
+        }, 666 )
+        
+    })
 })
---
+++ script/slides/animation.js
@@ -0,0 +1,25 @@
+import { TweenLite, TweenMax } from 'gsap'
+
+export const openLeftSlide = () => {
+    TweenLite.to(`.left-slider`, .5, {'left': 0})
+    TweenLite.to(`.left-slider`, .5, {'height': '100%'}).delay(.7)
+    TweenLite.fromTo(`.left-slide-mid, .left-slide-top, .left-slide-bot`, .5, { opacity:0, 'visibility':'hidden' }, { opacity: 1, 'visibility':'visible' },.5  ).delay(1.5)
+}
+
+export const closeLeftSlide = () => {
+    TweenLite.fromTo(`.left-slide-mid, .left-slide-top, .left-slide-bot`, .5, { opacity:1, 'visibility':'visible' }, { opacity: 0, 'visibility':'hidden' },.5  )
+    TweenLite.to(`.left-slider`, .5, {'height': '60%'}).delay(.7)
+    TweenLite.to(`.left-slider`, .5, {'left': '-95%'}).delay(1.5)
+}
+
+export const openRightSlide = () => {
+    TweenLite.to(`.right-slider`, .5, {'right': 0})
+    TweenLite.to(`.right-slider`, .5, {'height': '100%'}).delay(.7)
+    TweenLite.fromTo(`.right-slide-mid, .right-slide-top, .right-slide-bot`, .5, { opacity:0, 'visibility':'hidden' }, { opacity: 1, 'visibility':'visible' },.5  ).delay(1.5)
+}
+
+export const closeRightSlide = () => {
+    TweenLite.fromTo(`.right-slide-mid, .right-slide-top, .right-slide-bot`, .5, { opacity:1, 'visibility':'visible' }, { opacity: 0, 'visibility':'hidden' }, .5  )
+    TweenLite.to(`.right-slider`, .5, {'height': '60%'}).delay(.7)
+    TweenLite.to(`.right-slider`, .5, {'right': '-95%'}).delay(1.5)
+}
